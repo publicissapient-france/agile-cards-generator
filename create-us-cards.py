b2a6dac1834a32d40b5e736183008ff69440f648
Emmanuel Sciara <emmanuel.sciara@gmail.com>
2014-02-19 18:51:01 +0100

    now output directory is created if not existant and output file is removed if already existing

--- create-us-cards.py
+++ create-us-cards.py
@@ -1,5 +1,23 @@
+import os
+
 def main():
-    print('hello8!')
+    file_name = prepare_output_file('output', 'xlsx')
+    print(file_name)
+
+def prepare_output_file(output_file, extension):
+    file_name = None
+    if output_file is not None:
+        file_name = output_file
+    else:
+        file_name = 'output.' + extension
+    output_path = 'output'
+    if not os.path.isdir(output_path):
+        os.makedirs(output_path, exist_ok=True)
+    file_name = os.path.join(output_path, file_name)
+    if os.path.isfile(file_name):
+        os.remove(file_name)
+    return file_name
+
 
 if __name__ == "__main__":
     main()
